6131f7a850c7ecc9cbd1fc19c86b6c995e8a51d6
Kolinjo <kozicmarko@yahoo.com>
2019-10-13 00:02:03 +0200

    Exercise done

--- src/exercises/ella/Game.java
+++ src/exercises/ella/Game.java
@@ -88,221 +88,52 @@ public class Game implements MouseListener, ActionListener {
 	}
 
 	void createLevelOneButtons() {
-		try {
-			addButton("balloon.png", 698, 336);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
+			balloonButton = addButton("balloon.png", 698, 336);
+			diamondButton = addButton("diamond.png", 315, 900);
+			pawprintsButton = addButton("pawprints.png", 1020, 730);
+			ringButton = addButton("WeddingRing.png", 1200, 400);
+			snakeButton = addButton("snake.png", 1000, 350);
+			backpackButton = addButton("backpack.png", 640, 800);
+			sparklesButton = addButton("sparkles.png", 650, 750);
+	}
+
+	private JButton addButton(String fileName, int xPosition, int yPosition)  {
+		JButton button = new JButton();
 		
 		try {
-			addButton("diamond.png", 315, 900);
-
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image pawprints = ImageIO.read(getClass().getResource("pawprints.png"));
-			pawprintsButton = new JButton(new ImageIcon(pawprints));
+			Image balloon = ImageIO.read(getClass().getResource(fileName));
+			balloonButton = new JButton(new ImageIcon(balloon));
 			panel.setLayout(null);
-			panel.add(pawprintsButton);
-			pawprintsButton.addMouseListener(this);
-			pawprintsButton.setBorder(null);
-			pawprintsButton.setBounds(1079, 782, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
+			panel.add(balloonButton);
+			balloonButton.addMouseListener(this);
+			balloonButton.setBorder(null);
+			balloonButton.setBounds(xPosition, yPosition, 30, 30);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		try {
-			Image ring = ImageIO.read(getClass().getResource("WeddingRing.png"));
-			ringButton = new JButton(new ImageIcon(ring));
-			panel.setLayout(null);
-			panel.add(ringButton);
-			ringButton.addMouseListener(this);
-			ringButton.setBorder(null);
-			ringButton.setBounds(1460, 500, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image snake = ImageIO.read(getClass().getResource("snake.png"));
-			snakeButton = new JButton(new ImageIcon(snake));
-			panel.setLayout(null);
-			panel.add(snakeButton);
-			snakeButton.addMouseListener(this);
-			snakeButton.setBorder(null);
-			snakeButton.setBounds(1100, 420, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image backpack = ImageIO.read(getClass().getResource("backpack.png"));
-			backpackButton = new JButton(new ImageIcon(backpack));
-			panel.setLayout(null);
-			panel.add(backpackButton);
-			backpackButton.addMouseListener(this);
-			backpackButton.setBorder(null);
-			backpackButton.setBounds(840, 770, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image sparkles = ImageIO.read(getClass().getResource("sparkles.png"));
-			sparklesButton = new JButton(new ImageIcon(sparkles));
-			panel.setLayout(null);
-			panel.add(sparklesButton);
-			sparklesButton.addMouseListener(this);
-			sparklesButton.setBorder(null);
-			sparklesButton.setBounds(680, 756, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
+		return button;
+		
 	}
 
-	private void addButton(String fileName, int xPosition, int yPosition) throws IOException {
-		Image balloon = ImageIO.read(getClass().getResource(fileName));
-		balloonButton = new JButton(new ImageIcon(balloon));
-		panel.setLayout(null);
-		panel.add(balloonButton);
-		balloonButton.addMouseListener(this);
-		balloonButton.setBorder(null);
-		balloonButton.setBounds(xPosition, yPosition, 30, 30);
-	}
-
-	void levelTwoButtons() {
-
-		try {
-			Image apple = ImageIO.read(getClass().getResource("apple.png"));
-			appleButton = new JButton(new ImageIcon(apple));
-			panel.setLayout(null);
-			panel.add(appleButton);
-			appleButton.addMouseListener(this);
-			appleButton.setBorder(null);
-			appleButton.setBounds(503, 931, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image bow = ImageIO.read(getClass().getResource("bow.png"));
-			bowButton = new JButton(new ImageIcon(bow));
-			panel.setLayout(null);
-			panel.add(bowButton);
-			bowButton.addMouseListener(this);
-			bowButton.setBorder(null);
-			bowButton.setBounds(953, 903, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image cloud = ImageIO.read(getClass().getResource("cloud.png"));
-			cloudButton = new JButton(new ImageIcon(cloud));
-			panel.setLayout(null);
-			panel.add(cloudButton);
-			cloudButton.addMouseListener(this);
-			cloudButton.setBorder(null);
-			cloudButton.setBounds(460, 529, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image rose = ImageIO.read(getClass().getResource("rose.png"));
-			roseButton = new JButton(new ImageIcon(rose));
-			panel.setLayout(null);
-			panel.add(roseButton);
-			roseButton.addMouseListener(this);
-			roseButton.setBorder(null);
-			roseButton.setBounds(719, 664, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image chicken = ImageIO.read(getClass().getResource("chicken.png"));
-			chickenButton = new JButton(new ImageIcon(chicken));
-			panel.setLayout(null);
-			panel.add(chickenButton);
-			chickenButton.addMouseListener(this);
-			chickenButton.setBorder(null);
-			chickenButton.setBounds(351, 575, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image key = ImageIO.read(getClass().getResource("key.png"));
-			keyButton = new JButton(new ImageIcon(key));
-			panel.setLayout(null);
-			panel.add(keyButton);
-			keyButton.addMouseListener(this);
-			keyButton.setBorder(null);
-			keyButton.setBounds(953, 903, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image earth = ImageIO.read(getClass().getResource("earth.png"));
-			earthButton = new JButton(new ImageIcon(earth));
-			panel.setLayout(null);
-			panel.add(earthButton);
-			earthButton.addMouseListener(this);
-			earthButton.setBorder(null);
-			earthButton.setBounds(1224, 879, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
-		try {
-			Image cactus = ImageIO.read(getClass().getResource("cactus.png"));
-			cactusButton = new JButton(new ImageIcon(cactus));
-			panel.setLayout(null);
-			panel.add(cactusButton);
-			cactusButton.addMouseListener(this);
-			cactusButton.setBorder(null);
-			cactusButton.setBounds(578, 588, 30, 30);
-		} catch (Exception ex) {
-			System.out.println(ex);
-		}
+	void createLevelTwoButtons() {
+		
+		appleButton = addButton("apple.png", 500, 930);
+		bowButton = addButton("bow.png", 953, 903);
+		cloudButton = addButton("cloud.png", 460, 529);
+		roseButton = addButton("rose.png", 719, 664);
+		chickenButton = addButton("chicken.png", 350, 570);
+		earthButton = addButton("earth.png", 1220, 880);
+		cactusButton = addButton("cactus.png", 580, 590);
+		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+		
 		System.out.println(e.getX());
 		System.out.println(e.getY());
-		// if(e.getX() <= 10){
-		// ringName.setText("Found me!");
-		// ringName.setBounds(foundMeX, findMeY + 40, foundMeWidth,
-		// foundMeHeight);
-
-		// }
-		// if (e.getX() >= sparklesX - 15 && e.getX() <= sparklesX + 15 &&
-		// e.getY() <= sparklesY + 15 && e.getY() >= sparklesY - 15) {
-		// bar.foundSparkles();
-		// checkMe4 = true;
-		// } else if (e.getX() >= ringX - 15 && e.getX() <= ringX + 15 &&
-		// e.getY() <= ringY + 15 && e.getY() >= ringY - 15) {
-		// bar.foundRing();
-		// checkMe2 = true;
-		// } else if (e.getX() >= backpackX - 15 && e.getX() <= backpackX + 15
-		// && e.getY() <= backpackY + 15 && e.getY() >= backpackY - 15) {
-		// bar.foundBackpack();
-		// checkMe3 = true;
-		// } else if (e.getX() >= pawprintX - 15 && e.getX() <= pawprintX + 15
-		// && e.getY() <= pawprintY + 15 && e.getY() >= pawprintY - 15) {
-		// bar.foundPawprint();
-		// checkMe5 = true;
-		// } else if (e.getX() >= diamondX - 15 && e.getX() <= diamondX + 15 &&
-		// e.getY() <= diamondY + 15 && e.getY() >= diamondY - 15) {
-		// bar.foundDiamond();
-		// checkMe6 = true;
-		// } else if (e.getX() >= snakeX - 15 && e.getX() <= snakeX + 15 &&
-		// e.getY() <= snakeY + 15 && e.getY() >= snakeY - 15) {
-		// bar.foundSnake();
-		// checkMe = true;
-		// }
-		// if (checkMe == true && checkMe2 == true && checkMe3 == true &&
-		// checkMe4 == true && checkMe5 == true && checkMe6 == true) {
-		// JOptionPane.showMessageDialog(null, "Congratulations! You found all
-		// the pictures! \n Ready for level 2?");
-		// panel.nextLevelPicture();
-		// bar.nextLevelBar();
-		// gameNumber += 1;
-		// }
 
 	}
 
@@ -358,7 +189,7 @@ public class Game implements MouseListener, ActionListener {
 			checkMe5 = false;
 			checkMe6 = false;
 			checkMe7 = false;
-			levelTwoButtons();
+			createLevelTwoButtons();
 			time = 0;
 			timer.restart();
 
@@ -399,13 +230,18 @@ public class Game implements MouseListener, ActionListener {
 			bar.foundCactus();
 			checkMeL27 = true;
 		}
-		
-		if (checkMeL2 == true && checkMeL22 == true && checkMeL23 == true && checkMeL24 == true && checkMeL25 == true
-				&& checkMeL26 == true && checkMeL27 == true) {
+		if (checkIfAllFound())
+		 {
 			timer.stop();
 			JOptionPane.showMessageDialog(null, "Congratulations! You found all the pictures! \n You finished the puzzle in: " + time + " seconds.");
 			System.exit(0);
 		}
+	}
+	
+	private boolean checkIfAllFound () {
+		return checkMeL2 == true && checkMeL22 == true && checkMeL23 == true && checkMeL24 == true && checkMeL25 == true
+				&& checkMeL26 == true && checkMeL27 == true;
+		
 	}
 
 	private boolean checkFoundAll() {
